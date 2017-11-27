f67ca09f39b3dfb140053b23540a92ee40630b31
Nabil Freeman <nabil@instafork.com>
2017-11-27 17:14:08 +0000

    import deps

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const Axios = require('axios');
+const JSDom = require('jsdom');
+
 //store_slug is the last component of the URL where you normally look at your company.
 //for example, the following url:
 //https://www.reviews.io/company-reviews/store/euro-car-parts
@@ -5,5 +8,5 @@
 module.exports = async store_slug => {
 	const url = `https://www.reviews.io/company-reviews/store/${store_slug}`;
 
-	
+
 }
