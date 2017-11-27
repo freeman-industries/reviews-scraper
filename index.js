ef4cb62d289281c92277b64053040366fab53f72
Nabil Freeman <nabil@instafork.com>
2017-11-27 17:15:47 +0000

    fetch data from reviews.co.uk

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Axios = require('axios');
 const JSDom = require('jsdom');
 
+const fetchPage = async url => {
+	const response = await Axios({
+		method: 'get',
+		url
+	});
+
+	const html = response.data;
+
+	return html;
+}
+
 //store_slug is the last component of the URL where you normally look at your company.
 //for example, the following url:
 //https://www.reviews.io/company-reviews/store/euro-car-parts
@@ -8,5 +19,7 @@ const JSDom = require('jsdom');
 module.exports = async store_slug => {
 	const url = `https://www.reviews.io/company-reviews/store/${store_slug}`;
 
+	const html = await fetchPage(url);
 
+	return html;
 }
